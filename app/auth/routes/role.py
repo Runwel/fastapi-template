87b9e918ec824116afd113cf4688eb151ccbcd2d
Runwel <catre.ronweljohn.eccbsinfotech@gmail.com>
2025-04-29 16:53:52 +0800

    remobed authentication

--- app/auth/routes/role.py
+++ app/auth/routes/role.py
@@ -5,32 +5,32 @@ from app.auth.services.role_service import (
 )
 from app.auth.models.user import User
 from app.core.db.session import get_db
-from app.auth.dependencies import admin_or_dev_user
+# from app.auth.dependencies import admin_or_dev_user
 from app.auth.schemas.role import RoleCreateSchema
 
 router = APIRouter()
 
 @router.post("/")
-def add_role(role_data: RoleCreateSchema, db: Session = Depends(get_db), user: User = Depends(admin_or_dev_user)):
-    role = create_role(db, role_data.name, role_data.permissions, user)
+def add_role(role_data: RoleCreateSchema, db: Session = Depends(get_db)):
+    role = create_role(db, role_data.name, role_data.permissions)
     if not role:
         raise HTTPException(status_code=400, detail="Role already exists")
     return role
 
 @router.put("/{role_id}")
-def update_role(role_id: int, role_data: RoleCreateSchema, db: Session = Depends(get_db), user: User = Depends(admin_or_dev_user)):
-    role = update_role_data(db, role_id, role_data, user)
+def update_role(role_id: int, role_data: RoleCreateSchema, db: Session = Depends(get_db)):
+    role = update_role_data(db, role_id, role_data)
     if not role:
         raise HTTPException(status_code=404, detail="Role not found")
     return role
 
 @router.get("/")
-def get_roles(db: Session = Depends(get_db), user: User = Depends(admin_or_dev_user)):
-    return get_all_roles(db, user)
+def get_roles(db: Session = Depends(get_db)):
+    return get_all_roles(db)
 
 @router.delete("/{role_id}")
-def remove_role(role_id: int, db: Session = Depends(get_db), user: User = Depends(admin_or_dev_user)):
-    role = delete_role(db, role_id, user)
+def remove_role(role_id: int, db: Session = Depends(get_db)):
+    role = delete_role(db, role_id)
     if not role:
         raise HTTPException(status_code=404, detail="Role not found")
     return role
