9e44f6596ebb8c41d411316587172e2aab7e3d38
Runwel <catre.ronweljohn.eccbsinfotech@gmail.com>
2025-04-29 14:55:33 +0800

    Initial commit to personal repo

--- app/core/auth.py
+++ app/core/auth.py
@@ -6,7 +6,6 @@ load_dotenv(".env")
 
 X_TOKEN = os.environ["X_TOKEN"]
 
-
 async def verify_token(x_token: str = Header()):
     if x_token != X_TOKEN:
         raise HTTPException(status_code=400, detail="X-Token header invalid")
--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ from app.auth.routes.file import router as file_router
 from app.receipt_scanner.route import router as receipt_router
 from app.auth.routes.admin import router as admin_router
 from app.core.logger import init_logging
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv(".env")
 
@@ -17,6 +18,14 @@ app = FastAPI(title="FastAPI Boiler Plate")
 
 # Add database middleware
 app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://192.168.1.3:8080"],  # OR replace with ["http://your-machine-ip:5173"] for dev
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Register routes
 app.include_router(root_router)
